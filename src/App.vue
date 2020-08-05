92fa2263330156c6a9fead02692ca033d3b8ecba
sensactive <sensor14@list.ru>
2020-08-05 22:40:45 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main-table :rows="rows" :cols="cols"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import MainTable from '@/components/MainTable.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    MainTable,
   },
+  data: () => ({
+    rows: [
+      { text: 'Наименование', value: 'fullName' },
+      { text: 'Название', value: 'name' },
+      { text: 'Заголовок строки', value: 'titleRow' },
+      { text: 'Порядковый номер', value: 'number' },
+    ],
+    cols: [
+      {
+        fullName: '', name: '', titleRow: '', number: '1',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '2',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '3',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '4',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '5',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '6',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '7',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '8',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '9',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '10',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '11',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '12',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '13',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '14',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '15',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '16',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '17',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '18',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '19',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '20',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '21',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '22',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '23',
+      },
+      {
+        fullName: '', name: '', titleRow: '', number: '24',
+      },
+    ],
+  }),
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/MainTable.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="table-sc">
+    <div class="table__header"></div>
+    <div class="table__rows">
+      <div class="table__rows__first-col">
+        <div
+          v-for="(row, index) in rows"
+          :key="index + 'title'"
+          class="table__rows__first-col__cell"
+        >
+          {{ row.text }}
+        </div>
+      </div>
+      <div class="table__rows__content" ref="content">
+        <div v-for="(col, colIdx) in cols" :key="colIdx" class="table__rows__content-row">
+          <div
+            v-for="(row, rowIdx) in rows"
+            :key="rowIdx + 'cell'"
+            class="table__rows__content-row__cell"
+          >
+          <!--            <input type="text">-->
+            {{ col[row.value] }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainTable',
+  computed: {
+  },
+  props: {
+    rows: { type: Array, default: () => [] },
+    cols: { type: Array, default: () => [] },
+  },
+  mounted() {
+    this.$refs.content.style.gridTemplateColumns = `repeat(${this.cols.length}, max-content)`;
+  },
+};
+</script>
+
+<style lang="scss">
+  .table {
+    &-sc {
+      width: 1000px;
+      height: 500px;
+    }
+    &__rows {
+      display: grid;
+      grid-template-columns: 10vw max-content;
+      width: inherit;
+      &__content {
+        display: grid;
+        overflow: auto;
+        width: inherit;
+      }
+    }
+  }
+</style>
